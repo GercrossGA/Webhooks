4f81aafb59450de0e1c249d5bbbcabc336882d04
GercrossGA <gercross0@gmail.com>
2026-04-29 21:07:07 -0600

    actualizacion

--- appsrpingboot/demo/src/main/java/com/example/demo/DemoApplication.java
+++ appsrpingboot/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class DemoApplication {
+
+    @Value("${APP_GREETING:Hola}")
+    private String greeting;
+
+    @Value("${APP_ENVIRONMENT_INFO:}")
+    private String envInfo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -15,8 +22,12 @@ public class DemoApplication {
 	@Bean
     public CommandLineRunner commandLineRunner() {
         return args -> { 
-            System.out.println("¡Hello World desde AppSpringboot!");
+            System.out.println("************************");
+            System.out.println(greeting); 
+            if (!envInfo.isEmpty()) {
+                System.out.println(envInfo);
+            }
+            System.out.println("************************");
         };
     }
-
 }
